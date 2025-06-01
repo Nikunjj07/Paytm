10cfb1b7acd4be35c8e40631f6fdb04c6066252a
Nikunj <nikunjskabra@gmail.com>
2025-06-01 18:06:22 +0530

    updated auth

--- apps/paytm-client/app/lib/auth.ts
+++ apps/paytm-client/app/lib/auth.ts
@@ -1,6 +1,8 @@
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
 import db from "@repo/db/client"
+import { env } from "process";
+import { AsyncCallbackSet } from "next/dist/server/lib/async-callback-set";
 
 export const authOptions = {
     providers:[
@@ -10,10 +12,54 @@ export const authOptions = {
                 phone: { label: "Phone number", type: "text", placeholder: "1234567890", required: true },
                 password: { label: "Password", type: "password", required: true }
             },
-            async authorize(credentials:any){
-                const hashedPassword = await bcrypt.hash(credentials.password,10);
-                const existingUser = await db.user.findFirst({})
+            async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
+
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        email: existingUser.number
+                    }
+                }
+                return null;
             }
+
+            try {
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
         })
-    ]
+    ],
+    secret: process.env.JWT_SECRET || "",
+    callbacks:{
+        async session({token,session}:any){
+            session.user.id = token.sub
+
+            return session;
+        }
+    }
 }
