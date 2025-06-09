26ecca1f2f6180ed7feb95263fa0aedd2a2533ac
Nikunj <nikunjskabra@gmail.com>
2025-06-09 23:35:18 +0530

    updated dashboard

--- apps/paytm-client/app/(dashboard)/dashboard/page.tsx
+++ apps/paytm-client/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,21 @@
-export default ()=>{
+import { getServerSession } from "next-auth"
+import { authOptions } from "../../lib/auth"
+import prisma from "@repo/db/client";
+import { AsyncCallbackSet } from "next/dist/server/lib/async-callback-set";
+
+export default async()=>{
+    const session = await getServerSession(authOptions);
+    const user = session.user.name;
     return(
-        <div>
-            dashboard
+        <div className="">
+            
+            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                    Hello,&nbsp;
+                </span>
+                {user}
+            </h1>
+
         </div>
     )
 }
